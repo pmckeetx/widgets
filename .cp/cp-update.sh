d6069b0ea4d90ec544ba8d3ca0c1d12bb612de68
peter <peter.mckee@gmail.com>
2021-09-16 13:52:45 -0500

    fix url in cp-update.sh

--- .cp/cp-update.sh
+++ .cp/cp-update.sh
@@ -1,1 +1,1 @@
-curl http://localhost:8181/services/cp/widgets/https%3A%2F%2Fgithub.com%2Fpmckeetx%2Fwidgets
+curl http://cp.internal:8181/services/cp/widgets/https%3A%2F%2Fgithub.com%2Fpmckeetx%2Fwidgets
